320ff2719d9ef85eb1169a83f49d409d1b013e50
Eric Glover <eric24g@gmail.com>
2021-08-21 18:07:04 -0400

    added code to sum the total number of dice rolled from a random generatory

--- dice_roller.py
+++ dice_roller.py
@@ -1,6 +1,14 @@
-def primary():
-    print("Roll the dice")
+import random
 
+def primary():
+    dice_rolls = 2
+    dice_sum = 0
+    for i in range(0, dice_rolls):
+      roll = random.randint(1,6)
+      dice_sum += roll
+      print(f"You rolled a: {roll}")
+    print(f"You rolled a total of: {dice_sum}")
+        
 
 if __name__== "__main__":
     primary()
